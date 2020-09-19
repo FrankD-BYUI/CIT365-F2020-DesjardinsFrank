d0761759e7f63ecccdb51f5634f81cc413294ffc
FrankD-BYUI <fdesjardins@byui.edu>
2020-09-19 09:09:23 -0700

    W01 assignment complete

--- ConsoleApplication/ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+            // Step 2
             string name, location;
 
             Console.Write("What is your name: ");
@@ -15,9 +16,47 @@ namespace ConsoleApplication
             Console.Write("Where are you from: ");
             location = Console.ReadLine();
 
-            Console.WriteLine($"My name is {name}, I am from {location}.");
-            Console.WriteLine($"Todays date is {DateTime.Now.ToString("d", CultureInfo.CreateSpecificCulture("en-US"))}" +
-                $" and there are {12} days until Christmas.");
+            //Step 3
+            Console.WriteLine($"\nMy name is {name}, I am from {location}.");
+
+            // Steps 4 & 5
+            Console.Write("\nPress any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            DateTime today = DateTime.Now;
+            DateTime christmas = new DateTime(today.Year, 12, 25);
+            Console.WriteLine($"\nToday's date is {today.ToString("d", CultureInfo.CreateSpecificCulture("en-US"))}," +
+                $" and there are {(christmas - today).Days} days until Christmas.");
+
+            // Steps 6 & 7
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            Console.Write("\nPress any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            Console.WriteLine("\n~ ~ ~ Window Calculator ~ ~ ~");
+            Console.Write("Window width: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.Write("Window height: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            
+            woodLength = 2 * (width + height) * 3.25;
+            
+            glassArea = 2 * (width * height);
+            
+            Console.WriteLine("\nThe length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+            Console.WriteLine();
         }
+
     }
 }
