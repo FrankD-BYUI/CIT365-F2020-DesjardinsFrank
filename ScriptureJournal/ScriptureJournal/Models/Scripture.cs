debdc19910de5fb470aec9597651553d79e2cb5c
FrankD-BYUI <fdesjardins@byui.edu>
2020-10-31 19:19:32 -0700

    Adds minimum viable Scripture Journal app

---
+++ ScriptureJournal/ScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+        
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Book { get; set; }
+        
+        [Range(1, 200)]
+        [Required]
+        public int Chapter { get; set; }
+        
+        [Range(1, 200)]
+        [Required]
+        public int Verse { get; set; }
+        
+        [Display(Name = "Date Added")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime DateAdded { get; set; }
+        
+        public string Note { get; set; }
+    }
+}
---
+++ ScriptureJournal/ScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ScriptureJournal.Data;
+using ScriptureJournal.Models;
+
+namespace ScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ScriptureJournal.Data.ScriptureJournalContext _context;
+
+        public IndexModel(ScriptureJournal.Data.ScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        
+        public SelectList Books { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string BookFilter { get; set; }
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
+        {
+            IQueryable<string> bookQuery = from s in _context.Scripture
+                                           orderby s.Book
+                                           select s.Book;
+
+            var scriptures = from scripture in _context.Scripture
+                             select scripture;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                scriptures = scriptures.Where(y => y.Note.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(BookFilter))
+            {
+                scriptures = scriptures.Where(x => x.Book == BookFilter);
+            }
+            Books = new SelectList(await bookQuery.Distinct().ToListAsync());
+            Scripture = await scriptures.ToListAsync();
+        }
+    }
+}
